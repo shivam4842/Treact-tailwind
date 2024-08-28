2e983503ed914022b49a50ea6f8d1799219365ac
Shivam Goswami <shivamgoswami2357@gmail.com>
2024-08-28 13:15:11 +0530

    tailwind responsive changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
         'transparent': '#FFFFFF80',
         'pink-350': '#F687B3',
         'blue-935': '#506582',
+        'gray-350': '#E2E8F0',
+        'blue-80': '#EDF2F7',
       },
       fontFamily: {
         inter: ['Inter', 'system-ui'],
